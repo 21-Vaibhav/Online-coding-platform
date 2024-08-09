1064b3fe9c0bdf7d32eb74b80b56d837509377d1
likithkun <likith1238@gmail>
2024-08-10 01:02:25 +0530

    updated home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,9 +5,11 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800 mb-12">
-        Welcome to the Coding Platform
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
+      <h1 className="text-4xl font-bold text-gray-100 mb-12">
+        Welcome to   <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 animate-pulse">
+          Coding Backwards
+        </span>
       </h1>
       <div className="flex flex-col space-y-4">
       <button
